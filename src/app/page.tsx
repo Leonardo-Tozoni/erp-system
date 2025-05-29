0b9cae6aaf22dcbda532b2a590e6f28c91acb513
Leonardo Tozoni <leocebe2000@gmail.com>
2025-05-28 22:49:59 -0300

    feat: update ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SidebarMenu from "@/components/SidebarMenu/SidebarMenu";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen font-[family-name:var(--font-geist-sans)] bg-background">
+    <div className="flex items-center justify-center h-screen font-[family-name:var(--font-geist-sans)]">
       <SidebarMenu />
       {/* <LoginForm /> */}
     </div>
--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -8,19 +8,19 @@ import { Card, CardContent } from "../ui/card";
 
 export default function SidebarMenu () {
   return (
-    <Card className="fixed top-0 left-0 h-screen w-64 z-50 rounded-none shadow-none border-t border-gray-200">
+    <Card className="fixed top-0 left-0 h-screen w-64 z-50 rounded-none shadow-none bg-cyan-800">
       <CardContent className="p-5 flex flex-col items-center h-full">
         <Image alt="Barber Logo" src="/logo.png" height={36} width={120} />
 
         <div className="flex flex-col justify-between h-full w-full mt-8">
           <nav className="flex flex-col gap-4 w-full">
-            <Button variant="ghost" className="gap-2 w-full justify-start" asChild>
+            <Button variant="ghost" className="gap-2 w-full justify-start text-white" asChild>
               <Link href="/">
                 <HomeIcon size={18} />
                 Início
               </Link>
             </Button>
-            <Button variant="ghost" className="gap-2 w-full justify-start" asChild>
+            <Button variant="ghost" className="gap-2 w-full justify-start text-white" asChild>
               <Link href="/customer">
                 <BookPlus size={18}/>
                 Cadastro de Clientes
@@ -30,7 +30,7 @@ export default function SidebarMenu () {
 
           <div className="w-full mt-8">
             <div className="border-t border-gray-200 mb-4" />
-            <Button variant="ghost" className="gap-2 w-full justify-start">
+            <Button variant="ghost" className="gap-2 w-full justify-start text-white">
               <LogOutIcon size={18} />
               Sair
             </Button>
